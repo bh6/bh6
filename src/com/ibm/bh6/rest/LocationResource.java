4764e35d59233cd1848c0d7dc0f023c0e123fdd6
TobiasErnstberger <tobias.ernstberger@de.ibm.com>
2015-10-12 15:58:05 +0200

    Suche nach Location by Distance: http://bbhackathon6.mybluemix.net/api/location/byDistance?x=9.3&y=3

--- src/com/ibm/bh6/rest/LocationResource.java
+++ src/com/ibm/bh6/rest/LocationResource.java
@@ -1,12 +1,16 @@
 package com.ibm.bh6.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 @Path("/location")
@@ -17,14 +21,47 @@ import com.google.gson.JsonObject;
 public class LocationResource {
 
 	@GET
+	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response get(@QueryParam("id") long id) {
-		JsonObject resultObject = new JsonObject();
+	public Response get(@PathParam("id") String id) {
+
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty("id", id);
+		jsonObject.addProperty("name", "Mövenpick");
+		jsonObject.addProperty("type", "Hotel");
+		
+	
+		return Response.ok(jsonObject.toString()).build();
+		
+	}
+	
+	@GET
+	@Path("/byDistance")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getLocationByDistance(@QueryParam("x") double x,@QueryParam("x") double y) {
+		
+		JsonArray resultArray = new JsonArray();
+		JsonObject jsonObject;
+
+		jsonObject = new JsonObject();
+		jsonObject.addProperty("id", "1");
+		jsonObject.addProperty("name", "Mercure");
+		jsonObject.addProperty("type", "Hotel");
 		
 		
+		JsonObject gps = new JsonObject();
+		gps.addProperty("x", x);
+		gps.addProperty("y", y);
 		
-		return Response.ok(resultObject.toString()).build();
+		JsonObject adr = new JsonObject();
+		adr.addProperty("street", "Hauptstr.");
+		adr.addProperty("number", "3");
+		adr.addProperty("plz", "12345");
+		jsonObject.add("adress", adr);
 		
+		resultArray.add(jsonObject);
+		
+		return Response.ok(resultArray.toString()).build();
 	}
 	
 	
