1c5448fdaedb67791b6ad994612a6bec5b5f20e5
TobiasErnstberger <tobias.ernstberger@de.ibm.com>
2015-10-13 06:27:30 +0200

    Improvements for Session Management

--- src/com/ibm/bh6/rest/SessionResource.java
+++ src/com/ibm/bh6/rest/SessionResource.java
@@ -1,5 +1,7 @@
 package com.ibm.bh6.rest;
 
+import java.util.logging.Logger;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,31 +11,40 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.ibm.bh6.dao.DBUserQuery;
+import com.ibm.bh6.dao.impl.DBUserQueryImpl;
+import com.ibm.bh6.model.User;
+
 @Path("/session")
 public class SessionResource {
-	
+
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("/login")
-	public Response login(@Context HttpServletRequest request,@QueryParam("user") String user, @QueryParam("password") String password) {
-		if (checkLogin(request,user, password)) {
+	public Response login(@Context HttpServletRequest request, @QueryParam("user") String user,
+			@QueryParam("password") String password) {
+		if (checkLogin(request, user, password)) {
 			return Response.ok("You are now logged in").build();
 		}
 		performLogout(request);
 		return Response.ok("Loginfailed").build();
 
 	}
-	
+
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("/getCurrentUser")
 	public Response getCurrentUser(@Context HttpServletRequest request) {
-		String user = "";
-		
-		Object u = request.getSession().getAttribute("currentUser");
-		if (u!=null) user = (String) u;
-		
-		return Response.ok("User: "+user).build();
+		User u;
+
+		Object o = request.getSession().getAttribute("currentUser");
+		System.out.println(o);
+		if (o != null && o instanceof User) {
+			u = (User) o;
+			return Response.ok("User: " + u.getUserName()).build();
+		}
+
+		return Response.ok("No current User").build();
 	}
 
 	@GET
@@ -45,14 +56,20 @@ public class SessionResource {
 	}
 
 	private void performLogout(HttpServletRequest request) {
+		request.getSession().removeAttribute("currentUser");
 		request.getSession().invalidate();
 	}
 
-	private boolean checkLogin(HttpServletRequest request,String user, String password) {
-		request.getSession().setAttribute("currentUser", user);
-		return true;
+	private boolean checkLogin(HttpServletRequest request, String user, String password) {
+
+		DBUserQuery db = new DBUserQueryImpl();
+		User u = db.getUser(user); // TODO Noch eine Passwort prüfung
+		System.out.println("User: "+u);
+		if (u != null && u instanceof User) {
+			request.getSession().setAttribute("currentUser", u);
+			return true;
+		}
+		return false;
 	}
-	
-	
 
 }
